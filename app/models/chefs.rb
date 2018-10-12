af57f52905672eadcff2a61dcb741d4c96104eae
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-12 21:38:30 +0000

    adds tables, adds slug method for chefs/users

--- app/models/chefs.rb
+++ app/models/chefs.rb
@@ -1,4 +1,11 @@
 class Chef < ActiveRecord::Base
   has_many :dishes
 
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Chef.all.find{|chef| chef.slug == slug}
+  end
 end
--- db/migrate/20181012030650_create_dishes.rb
+++ db/migrate/20181012030650_create_dishes.rb
@@ -1,4 +1,8 @@
 class CreateDishes < ActiveRecord::Migration
   def change
+    create_table :dishes do |t|
+      t.string :name
+      t.integer :chef_id
+    end
   end
 end
