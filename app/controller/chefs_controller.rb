4ada254bd938386c45c42f8bedf6c81deacb6052
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-13 04:11:14 +0000

    added methods for chefs controller

--- app/controller/chefs_controller.rb
+++ app/controller/chefs_controller.rb
@@ -1,3 +1,53 @@
 class Chefscontroller < ApplicationController
 
+  get '/chefs/:slug' do
+    @chef = Chef.find_by_slug(params[:slug])
+    erb :'/chefs/show'
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect to '/dishes'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+     redirect to '/signup'
+   else
+     @chef = Chef.new(:username => params[:username], :email => params[:email], :password => params[:password])
+     @chef.save
+     session[:chef_id] = @chef.id
+     redirect to '/dishes'
+   end
+  end
+
+  get '/login' do
+      if !logged_in?
+        erb :'chef/login'
+      else
+        redirect to '/dishes'
+      end
+    end
+
+    post '/login' do
+      chef = Chef.find_by(:username => params[:username])
+      if chef && chef.authenticate(params[:password])
+        session[:chef_id] = chef.id
+        redirect to "/dishes"
+      else
+        redirect to '/signup'
+      end
+    end
+
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        redirect to '/login'
+      else
+        redirect to '/'
+      end
+  end
 end
--- app/controller/dishes_controller.rb
+++ app/controller/dishes_controller.rb
@@ -1,5 +1,7 @@
 class DishesController < ApplicationController
 
+  get '/dishes' do
 
+  end
 
 end
