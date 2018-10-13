30259d007bcb2c995bf857ad71a799c1ee13c870
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-10-13 03:26:56 +0000

    adds helper methods

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -12,4 +12,14 @@ class ApplicationController < Sinatra::Base
       index.erb
     end
 
+    helpers do
+      def logged_in?
+        !!current_user
+      end
+
+      def current_user
+         @current_user ||= Chef.find_by(id: session[:chef_id]) if session[:chef_id]
+      end
+    end
+
 end
